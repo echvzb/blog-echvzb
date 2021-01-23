f967b2a9ff7815d70019d5c9c7037d1e9fc785a8
eduardochvzb <eduardochvzb@gmail.com>
2021-01-23 13:21:55 -0600

    2.0.1

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
 import Navigation from "../components/navigation";
 import 'prismjs/themes/prism-okaidia.css';
-import Helmet from 'react-helmet';
 
 export default ({ children }) => {
   const data = useStaticQuery(
@@ -17,24 +16,22 @@ export default ({ children }) => {
     `
   )
   return (
-    <div>
-      <div className="site-wrapper">
-        <header className="site-header">
-          <div className="site-title">
-            <Link to="/">{data.site.siteMetadata.title}</Link>
+    <div className="site-wrapper">
+      <header className="site-header">
+        <div className="site-title">
+          <Link to="/">{data.site.siteMetadata.title}</Link>
+        </div>
+        <div className='border-logo-grd'>
+          <div className='logo-header-container'>
+            <div className='logo-header'></div>
           </div>
-          <div className='border-logo-grd'>
-            <div className='logo-header-container'>
-              <div className='logo-header'></div>
-            </div>
-          </div>
-          <Navigation />
-        </header>
-        {children}
-        <footer className="site-footer">
-          <p>Made with <span role='img' aria-label="heart">&#10084;&#65039;</span> by &copy; {new Date().getFullYear()} <a href='https://echvzb.me'>echvzb</a></p>
-        </footer>
-      </div>
+        </div>
+        <Navigation />
+      </header>
+      {children}
+      <footer className="site-footer">
+        <p>Made with <span role='img' aria-label="heart">&#10084;&#65039;</span> by &copy; {new Date().getFullYear()} <a href='https://echvzb.me'>echvzb</a></p>
+      </footer>
     </div>
   )
 }
