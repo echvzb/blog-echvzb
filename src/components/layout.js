d0ea2e8fb2dc3fa587a19c18d192b57ec0fe3a55
eduardochvzb <eduardochvzb@gmail.com>
2021-02-04 16:12:19 -0600

    buy me a coffee in body

--- src/components/layout.js
+++ src/components/layout.js
@@ -17,18 +17,6 @@ export default ({ children }) => {
   )
   return (
     <div className="site-wrapper">
-      <script
-          data-name="BMC-Widget"
-          data-cfasync="false"
-          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-          data-id="echvzb"
-          data-description="Support me on Buy me a coffee!"
-          data-message="Thank you for visiting. You can now buy me a coffee!"
-          data-color="#5F7FFF"
-          data-position="right"
-          data-x_margin="28"
-          data-y_margin="18"
-        />
       <header className="site-header">
         <div className="site-title">
           <Link to="/">{data.site.siteMetadata.title}</Link>
--- src/html.js
+++ src/html.js
@@ -22,6 +22,18 @@ export default function HTML(props) {
           id="___gatsby"
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
+        <script
+          data-name="BMC-Widget"
+          data-cfasync="false"
+          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+          data-id="echvzb"
+          data-description="Support me on Buy me a coffee!"
+          data-message="Thank you for visiting. You can now buy me a coffee!"
+          data-color="#5F7FFF"
+          data-position="right"
+          data-x_margin="28"
+          data-y_margin="18"
+        />
         {props.postBodyComponents}
       </body>
     </html>
